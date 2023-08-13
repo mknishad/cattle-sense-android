66e6dc57e008c5a9dfaf04ee19310aeb80154101
Monir <mknishad1@ymail.com>
2023-08-14 00:46:42 +0600

    add and design cattle info activity

--- app/src/main/java/com/datasense/cattlesense/ui/activity/MainActivity.kt
+++ app/src/main/java/com/datasense/cattlesense/ui/activity/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     binding.cattleInfoLayout.setOnClickListener {
-
+      startActivity(Intent(this, CattleInfoActivity::class.java))
     }
 
     binding.cattleHealthLayout.setOnClickListener {
