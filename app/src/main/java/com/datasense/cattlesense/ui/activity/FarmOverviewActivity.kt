cdfcae172a294d5b69d549c46b7304d0d3596b86
Monir <mknishad1@ymail.com>
2023-08-25 21:44:30 +0600

    add and design farm overview activity

---
+++ app/src/main/java/com/datasense/cattlesense/ui/activity/FarmOverviewActivity.kt
@@ -0,0 +1,28 @@
+package com.datasense.cattlesense.ui.activity
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.datasense.cattlesense.R
+import com.datasense.cattlesense.databinding.ActivityFarmOverviewBinding
+
+class FarmOverviewActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityFarmOverviewBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityFarmOverviewBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    supportActionBar?.title = getString(R.string.farm_name)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (item.itemId == android.R.id.home) {
+      finish()
+      return true
+    }
+    return super.onOptionsItemSelected(item)
+  }
+}
